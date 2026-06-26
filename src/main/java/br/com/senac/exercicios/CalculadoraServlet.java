874d50761325143ecb46c54cc6447492cb208ff7
Stefanie Reetz <stefaniebeatrizreetz@gmail.com>
2026-06-25 21:12:56 -0300

    trocado para getParameter em SaudacaoServlet

--- src/main/java/br/com/senac/exercicios/CalculadoraServlet.java
+++ src/main/java/br/com/senac/exercicios/CalculadoraServlet.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-
+// http://localhost:8080/java-web-demo/calculadora?n1=1&n2=2&operacao=soma
 @WebServlet("/calculadora")
 public class CalculadoraServlet extends HttpServlet {
 
@@ -19,7 +19,7 @@ public class CalculadoraServlet extends HttpServlet {
         double n1= Double.parseDouble(req.getParameter("n1"));
         double n2= Double.parseDouble(req.getParameter("n2"));
         String operacao = req.getParameter("operacao");
-        double resultado;
+        double resultado = 0;
 
         switch (operacao){
             case "soma":
@@ -29,19 +29,28 @@ public class CalculadoraServlet extends HttpServlet {
                 break;
 
             case "subtracao":
+                resultado = n1 - n2;
                 break;
 
             case "multiplicacao":
+                resultado = n1 * n2;
                 break;
 
             case "divisao":
+                if (n2 == 0){
+                    resp.getWriter().println("<h1>nao da pra dividir por zero </h1>");
+                } else {
+                    resultado = n1 / n2;
+                }
+
                 break;
 
             default:
+                resp.getWriter().println("<h1> Operação desconhecida </h1>");
                 break;
         }
 
-        resp.getWriter().println();
+        resp.getWriter().println("<h1> " + resultado + " </h1>");
 
 
     }
